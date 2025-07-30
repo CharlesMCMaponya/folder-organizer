623fca4fcff4ae8248f3b2c06b33964b9717d38d
Charles Mosehla Maponya <charlesmaponya64@gmail.com>
2025-07-30 22:28:21 +0200

    Update README.md and organize.sh script

--- organize.sh
+++ organize.sh
@@ -22,7 +22,7 @@ for file in "$DOWNLOADS"/*; do
 
         case "$extension" in
             jpg|jpeg|png|gif|bmp)
-                echo "🖼 Moving $filename to images/"
+                echo " Moving $filename to images/"
                 mv "$file" "$IMAGES"
                 ;;
             pdf|doc|docx|txt|xls|xlsx|ppt|pptx)
